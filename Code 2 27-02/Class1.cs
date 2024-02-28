2b2b54f01919c6152856e9f55c0ea517a697709e
arria <arria@NAU>
2024-02-27 22:30:16 -0600

    Agregar archivos de proyecto.

---
+++ Code 2 27-02/Class1.cs
@@ -0,0 +1,32 @@
+﻿class BasicClass
+{
+    private int integerValue;
+    private string stringValue;
+
+    // Default constructor
+    public BasicClass()
+    {
+        Console.WriteLine("Default constructor called.");
+    }
+
+    // Parameterized constructor
+    public BasicClass(int intValue, string strValue)
+    {
+        integerValue = intValue;
+        stringValue = strValue;
+        Console.WriteLine("Parameterized constructor called.");
+    }
+
+    // Destructor
+    ~BasicClass()
+    {
+        Console.WriteLine("Destructor called.");
+    }
+
+    // Method to display values
+    public void DisplayValues()
+    {
+        Console.WriteLine($"Integer Value: {integerValue}, String Value: {stringValue}");
+    }
+}
+
---
+++ Code 2 27-02/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Example of using the BasicClass
+        Console.WriteLine("Creating object using default constructor:");
+        BasicClass obj1 = new BasicClass();
+        obj1.DisplayValues();
+
+        Console.WriteLine("\nCreating object using parameterized constructor:");
+        BasicClass obj2 = new BasicClass(27, "pq");
+        obj2.DisplayValues();
+
+        // The destructors will be called when objects go out of scope or when explicitly set to null
+        obj1 = null;
+        obj2 = null;
+        GC.Collect(); // Force garbage collection
+    }
+}
